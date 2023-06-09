a8e5658d0c5f45026b42a5ba4c28fa51e6969de4
quanjingchen <quanjingchen@gmail.com>
2023-06-08 18:56:30 -0700

    change text

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello()
 {
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. hahhahaha </h1>";
+return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. hahhahaha George is the most adorable dog </h1>";
 }
 }
